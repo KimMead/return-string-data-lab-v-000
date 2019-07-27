772b13c279c9f781ce1fc6123ab42db7f67271e9
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2019-07-27 00:18:54 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -15,4 +15,9 @@ class ProductsController < ApplicationController
    @product.save
    redirect_to products_path
   end
+
+  def description
+    product = Product.find(params[:id])
+    render plain: product.description
+  end
 end
