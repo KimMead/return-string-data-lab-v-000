fbb423f587791a94d92a4b8a0f8269d9f7944b9a
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2019-07-27 00:17:40 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,4 +5,14 @@ class ProductsController < ApplicationController
 
   def show
   end
+
+  def new
+   @product = Product.new
+  end
+
+  def create
+   @product = Product.create(product_params)
+   @product.save
+   redirect_to products_path
+  end  
 end
